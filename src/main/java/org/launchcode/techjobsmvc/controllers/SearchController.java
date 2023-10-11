d981246e8a004ae1811b42b3bba0cd42a35ab507
Yumi <ydshiroma@gmail.com>
2023-10-11 15:13:44 -0400

    added text to show search term

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -32,17 +32,16 @@ public class SearchController {
     // TODO #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam(required = false) String searchTerm) {
-//        model.addAttribute("searchType", searchType);
-//        model.addAttribute("searchTerm", searchTerm);
-
         ArrayList<Job> jobs;
         if (searchTerm.equals("all") || isNull(searchTerm)){
             jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
         } else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
         }
-        model.addAttribute("jobs", jobs);
 
+        model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
 
         return "search";
