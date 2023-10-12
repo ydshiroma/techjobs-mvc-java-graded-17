4bed0393c6151de4119c2e1f39dd54636762764a
Yumi <ydshiroma@gmail.com>
2023-10-12 14:08:46 -0400

    super bonus mission part 6

--- src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/ListController.java
@@ -19,16 +19,9 @@ import java.util.LinkedHashMap;
 @RequestMapping(value = "list")
 public class ListController extends TechJobsController {
 
-    static HashMap<String, String> columnChoices = new HashMap<>();
     static HashMap<String, Object> tableChoices = new HashMap<>();
 
     public ListController () {
-        columnChoices.put("all", "All");
-        columnChoices.put("employer", "Employer");
-        columnChoices.put("location", "Location");
-        columnChoices.put("positionType", "Position Type");
-        columnChoices.put("coreCompetency", "Skill");
-
         tableChoices.put("all", "View All");
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
@@ -38,7 +31,7 @@ public class ListController extends TechJobsController {
 
     @GetMapping(value = "")
     public String list(Model model) {
-        model.addAttribute("columns", columnChoices);
+//        model.addAttribute("columns", columnChoices);
         model.addAttribute("tableChoices", tableChoices);
         model.addAttribute("employers", JobData.getAllEmployers());
         model.addAttribute("locations", JobData.getAllLocations());
--- src/main/java/org/launchcode/techjobsmvc/controllers/TechJobsController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/TechJobsController.java
@@ -6,15 +6,27 @@ import java.util.HashMap;
 
 public class TechJobsController {
     static HashMap<String, String> actionChoices = new HashMap<>();
+    static HashMap<String, String> columnChoices = new HashMap<>();
 
     public TechJobsController() {
         actionChoices.put("search", "Search");
         actionChoices.put("list", "List");
+
+        columnChoices.put("all", "All");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("location", "Location");
+        columnChoices.put("positionType", "Position Type");
+        columnChoices.put("coreCompetency", "Skill");
     }
 
     @ModelAttribute("actions")
     static HashMap<String, String> getActionChoices() {
         return actionChoices;
+    }
+
+    @ModelAttribute("columns")
+    static HashMap<String, String> getColumnChoices() {
+        return columnChoices;
     }
 
 }
